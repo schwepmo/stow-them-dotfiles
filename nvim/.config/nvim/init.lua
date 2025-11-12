e8b632d69f1e4a16fea3069282f806be2ae0e7d9
Moritz Schweppenhaeuser <moritz.schweppenhauser@gmail.com>
2025-11-12 13:57:23 +0100

    some updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -710,12 +710,13 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'latex' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
