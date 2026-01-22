dfa311481e8a6202a6e5a1ae1c4d29469e634cca
Moritz Schweppenhaeuser <moritz.schweppenhaeuser@fokus.fraunhofer.de>
2026-01-22 10:55:55 +0100

    chore: made treesitter use main branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
     main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
